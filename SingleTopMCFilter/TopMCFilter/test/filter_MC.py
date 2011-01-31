c1635984db95523dc8d2a32e0022ccfd47f24190
jyothsna <>
2011-01-31 23:49:04 +0000

    Fix the EDFilter name.

--- SingleTopMCFilter/TopMCFilter/test/filter_MC.py
+++ SingleTopMCFilter/TopMCFilter/test/filter_MC.py
@@ -20,7 +20,7 @@ process.source = cms.Source("PoolSource",
 process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
 #process.genMaker = cms.Path( process.genParticles )
 
-process.topTruthFilter = cms.EDFilter("TopFilter",
+process.topTruthFilter = cms.EDFilter("TopMCLepJetFilter",
                                       truthVector = cms.InputTag("genParticles"),
                                       jetsVector = cms.InputTag("kt4GenJets"),
                                       daughterIds = cms.vint32(11), #PDGID is: 11 for electrons and 13 for muons
